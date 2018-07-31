b11a37a07d65ef1f63a1e00c0b7d9c74a1d87ab3
ithiris <ithirisshabi@gmail.com>
2018-07-31 07:45:35 +0530

    sourcefile read

---
+++ brent.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var path = require("path");
+var http = require("http");
+var jsonFile = 'brentconfig.json';
+
+fs.readFile(jsonFile, 'utf8', function (err, jsonfile) {
+    if (err) {
+        console.log("error on the Json file!");
+        return;
+    }
+
+    var strToJson = JSON.parse(jsonfile);
+    var sourceFolder = strToJson.source;
+    readSourceFolderFiles ()
+})
+
+function readSourceFolderFiles(sourceFolder,files) {
+    var sourceallfiles = "";
+    fs.readdir(sourceFolder, 'utf8', function (err, files) {
+        if (err) {
+
+            console.log('error on fetching directoryFile!');
+            return
+        }
+
+
+        for (var i = 0; i < files.length; i++) {
+
+            var filePath = path.join(sourceFolder, files[i]);
+
+
+            fs.readFile(filePath, 'utf8', function (err, filedata) {
+                if (err) {
+
+                    console.log('error on fetching SourceFile!');
+                    return
+                }
+
+                sourceallfiles += filedata + "\n";
+                cb(sourceallfiles);
+            });
+        }
+
+    });
+}
